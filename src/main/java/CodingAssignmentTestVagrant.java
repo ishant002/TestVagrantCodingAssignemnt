fb759dfc307fd417722daa935444dc06fa69f1df
ishant.gupta <ishant.gupta@ril.com>
2023-07-04 16:14:07 +0530

    assignemnt completed

---
+++ src/main/java/CodingAssignmentTestVagrant.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class CodingAssignmentTestVagrant {
+    private int capacity;
+    private final Map<String, LinkedList<String>> songUserMap;
+
+    public CodingAssignmentTestVagrant(int capacity) {
+        this.capacity = capacity;
+        this.songUserMap = new HashMap<>();
+    }
+
+    public void playSong(String user, String song) {
+        if (!songUserMap.containsKey(user)) {
+            songUserMap.put(user, new LinkedList<>());
+        }
+
+        LinkedList<String> songs = songUserMap.get(user);
+
+        // Remove the song if it's already present in the user's playlist
+        songs.remove(song);
+
+        // Add the song at the beginning of the user's playlist
+        songs.addLast(song);
+
+        // Remove the least recently played song if the playlist exceeds the capacity
+        if (songs.size() > capacity) {
+            songs.removeFirst();
+        }
+    }
+
+    public List<String> getRecentlyPlayedSongs(String user) {
+        return songUserMap.getOrDefault(user, new LinkedList<>());
+    }
+}
---
+++ src/test/java/CodingAssignmentTests.java
@@ -0,0 +1,66 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CodingAssignmentTests {
+
+    @Test
+    public void testPlaySongAndGetRecentlyPlayedSongs() {
+        CodingAssignmentTestVagrant store = new CodingAssignmentTestVagrant(3);
+
+        // Test for a single user
+        store.playSong("User1", "S1");
+        store.playSong("User1", "S2");
+        store.playSong("User1", "S3");
+        List<String> user1Songs = store.getRecentlyPlayedSongs("User1");
+        Assert.assertEquals(3, user1Songs.size());
+        Assert.assertEquals("S3", user1Songs.get(2));
+        Assert.assertEquals("S2", user1Songs.get(1));
+        Assert.assertEquals("S1", user1Songs.get(0));
+
+        // Test for another user
+        store.playSong("User2", "S4");
+        List<String> user2Songs = store.getRecentlyPlayedSongs("User2");
+        Assert.assertEquals(1, user2Songs.size());
+        Assert.assertEquals("S4", user2Songs.get(0));
+
+        // Test playing a song already in the playlist
+        store.playSong("User1", "S2");
+        user1Songs = store.getRecentlyPlayedSongs("User1");
+        Assert.assertEquals(3, user1Songs.size());
+        Assert.assertEquals("S2", user1Songs.get(2));
+        Assert.assertEquals("S3", user1Songs.get(1));
+        Assert.assertEquals("S1", user1Songs.get(0));
+
+        // Test playing a song exceeding the capacity
+        store.playSong("User1", "S5");
+        user1Songs = store.getRecentlyPlayedSongs("User1");
+        Assert.assertEquals(3, user1Songs.size());
+        Assert.assertEquals("S5", user1Songs.get(2));
+        Assert.assertEquals("S2", user1Songs.get(1));
+        Assert.assertEquals("S3", user1Songs.get(0));
+
+        // Test for a user without any played songs
+        List<String> user3Songs = store.getRecentlyPlayedSongs("User3");
+        Assert.assertEquals(0, user3Songs.size());
+    }
+
+    @Test
+    public void testCapacityZero() {
+        CodingAssignmentTestVagrant store = new CodingAssignmentTestVagrant(0);
+
+        store.playSong("User1", "S1");
+        List<String> user1Songs = store.getRecentlyPlayedSongs("User1");
+        Assert.assertEquals(0, user1Songs.size());
+    }
+
+    @Test
+    public void testNegativeCapacity() {
+        CodingAssignmentTestVagrant store = new CodingAssignmentTestVagrant(-1);
+
+        store.playSong("User1", "S1");
+        List<String> user1Songs = store.getRecentlyPlayedSongs("User1");
+        Assert.assertEquals(0, user1Songs.size());
+    }
+}
